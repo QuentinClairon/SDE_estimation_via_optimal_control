acb8aa9666ff36728805fcb4723eae8092e40ab9
QuentinClairon <62789270+QuentinClairon@users.noreply.github.com>
2022-02-26 12:44:47 +0100

    Update hypo_fhn_matA_inhomogen.R

--- SDE_estimation_via_optimal_control/function_model_and_sims/hypo_fhn_matA_inhomogen.R
+++ SDE_estimation_via_optimal_control/function_model_and_sims/hypo_fhn_matA_inhomogen.R
@@ -9,8 +9,7 @@ hypo_fhn_matA_inhomogen <- function(t,State,theta){
   
   Mat_A[1,1] = (1-State[1]^2)*inv_eps
   Mat_A[1,2] = -inv_eps    
- # Mat_A[1,3] = 0
-  
+ 
   Mat_A[2,1] = gamma
   Mat_A[2,2] = -1
   #  Mat_A[2,3] = beta
